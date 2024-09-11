e5a7ff565b892239c8cbdd6d100e32ab6d651941
yuxp <1039354317@qq.com>
2024-09-11 16:20:16 +0800

    feat: add something

---
+++ .nx/cache/1435973143890581760/outputs/packages/remixapp/build/index.js
@@ -0,0 +1,114 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: !0 });
+}, __copyProps = (to, from, except, desc) => {
+  if (from && typeof from == "object" || typeof from == "function")
+    for (let key of __getOwnPropNames(from))
+      !__hasOwnProp.call(to, key) && key !== except && __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: !0 }), mod);
+
+// <stdin>
+var stdin_exports = {};
+__export(stdin_exports, {
+  assets: () => assets_manifest_default,
+  assetsBuildDirectory: () => assetsBuildDirectory,
+  entry: () => entry,
+  future: () => future,
+  publicPath: () => publicPath,
+  routes: () => routes
+});
+module.exports = __toCommonJS(stdin_exports);
+
+// app/entry.server.tsx
+var entry_server_exports = {};
+__export(entry_server_exports, {
+  default: () => handleRequest
+});
+var import_react = require("@remix-run/react"), import_server = require("react-dom/server"), import_jsx_runtime = require("react/jsx-runtime");
+function handleRequest(request, responseStatusCode, responseHeaders, remixContext) {
+  let markup = (0, import_server.renderToString)(
+    /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react.RemixServer, { context: remixContext, url: request.url })
+  );
+  return responseHeaders.set("Content-Type", "text/html"), new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders
+  });
+}
+
+// app/root.tsx
+var root_exports = {};
+__export(root_exports, {
+  default: () => App,
+  meta: () => meta
+});
+var import_react2 = require("@remix-run/react"), import_jsx_runtime2 = require("react/jsx-runtime"), meta = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1"
+});
+function App() {
+  return /* @__PURE__ */ (0, import_jsx_runtime2.jsxs)("html", { lang: "en", children: [
+    /* @__PURE__ */ (0, import_jsx_runtime2.jsxs)("head", { children: [
+      /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_react2.Meta, {}),
+      /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_react2.Links, {})
+    ] }),
+    /* @__PURE__ */ (0, import_jsx_runtime2.jsxs)("body", { children: [
+      /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_react2.Outlet, {}),
+      /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_react2.ScrollRestoration, {}),
+      /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_react2.Scripts, {}),
+      /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(import_react2.LiveReload, {})
+    ] })
+  ] });
+}
+
+// app/routes/index.tsx
+var routes_exports = {};
+__export(routes_exports, {
+  default: () => Index
+});
+var import_header = require("header"), import_footer = require("footer"), import_jsx_runtime3 = require("react/jsx-runtime");
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)(import_jsx_runtime3.Fragment, { children: [
+    /* @__PURE__ */ (0, import_jsx_runtime3.jsx)(import_header.Header, {}),
+    /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("div", { children: "Content!" }),
+    /* @__PURE__ */ (0, import_jsx_runtime3.jsx)(import_footer.Footer, {})
+  ] });
+}
+
+// server-assets-manifest:@remix-run/dev/assets-manifest
+var assets_manifest_default = { entry: { module: "/build/entry.client-Z4H7MEEE.js", imports: ["/build/_shared/chunk-HWY3JQXB.js", "/build/_shared/chunk-DOCVKONM.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-UCYABZTQ.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-HSRE7CY4.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, version: "bd94c703", hmr: void 0, url: "/build/manifest-BD94C703.js" };
+
+// server-entry-module:@remix-run/dev/server-build
+var assetsBuildDirectory = "public/build", future = { v2_dev: !1, unstable_postcss: !1, unstable_tailwind: !1, v2_errorBoundary: !1, v2_headers: !1, v2_meta: !1, v2_normalizeFormMethod: !1, v2_routeConvention: !1 }, publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
+  root: {
+    id: "root",
+    parentId: void 0,
+    path: "",
+    index: void 0,
+    caseSensitive: void 0,
+    module: root_exports
+  },
+  "routes/index": {
+    id: "routes/index",
+    parentId: "root",
+    path: void 0,
+    index: !0,
+    caseSensitive: void 0,
+    module: routes_exports
+  }
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  assets,
+  assetsBuildDirectory,
+  entry,
+  future,
+  publicPath,
+  routes
+});
