5594d7eb35b95c4d9158b441449ab65c7dc5bf93
yuxp <1039354317@qq.com>
2024-09-11 19:26:32 +0800

    feat: add something

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo toolddd----111!
+            Lerna is the original monorepo tool!
         </header>
     );
 }
